f8d41d52a5afd17f42592d8123d0cf0a67afe36e
rushali2607 <89059197+rushali2607@users.noreply.github.com>
2023-11-10 09:49:39 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 # flask is the module with small f letter and Flask is the class of this module with capital F and now we will create an object for this class as python is a object oriented programming language.
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "hello world"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
